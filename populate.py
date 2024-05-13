bec8668b7823a4cd6f39079b35ea8c9603cbf84e
CappyGerald <geraldodhiambo67@gmail.com>
2024-05-13 10:38:33 +0300

    Update populate.py

--- populate.py
+++ populate.py
@@ -13,12 +13,28 @@ topics_data = [
         "questions": [
             "As recently dramatized in a critically acclaimed miniseries, what year did the Chernobyl disaster occur?",
             "Who was Lord Mayor of London four times between 1397 and 1419 and the inspiration for a classic English folk tale?",
-            # Add more questions as needed
+              "Who was the second President of the United States?",
+               "Which British archaeologist discovered Tutankhamun's tomb?",
+               "Who was the leader of Britain's ill-fated Antarctic expedition that was one of the first to reach the South Pole in 1912?",
+                "In which European country was there a civil war between 1946 and 1949?",
+                 "Which 13th-century Scottish knight did Mel Gibson portray in 'Braveheart'?",
+                "Which war was fought in South Africa between 1899 and 1902?",
+                "In which country did the Second World War Battles of El Alamein take place?",
+               "Who discovered the wreckage of the Titanic?"
+           
         ],
         "answers": [
             "1986",
             "Richard (Dick) Whittington",
-            # Add corresponding answers
+              "John Adams",
+              "Howard Carter",
+                "Robert Falcon Scott",
+             "Greece",
+            "William Wallace",
+           "Anglo-Boer War",
+            "Egypt",
+            "Robert Ballard"
+           
         ]
     },
     {
@@ -26,12 +42,28 @@ topics_data = [
         "questions": [
             "Who developed the Python programming language?",
             "Which type of programming does Python support?",
-            # Add more questions as needed
+             "Is Python case-sensitive when dealing with identifiers?",
+            "What is the correct extension for Python files?",
+             "Is Python code compiled or interpreted?",
+           "What are all the keywords in Python written in?",
+           "What will be the output of 4 + 3 % 5?",
+              "Which keyword is used for defining a block of code in Python?",
+           "Which function is used for creating anonymous functions at runtime?",
+           "What does 'pip' stand for in Python?"
+            
         ],
         "answers": [
             "Guido van Rossum",
             "All of the mentioned",
-            # Add corresponding answers
+               "Yes",
+              ".py",
+               "Python code is both compiled and interpreted",
+               "Capitalized",
+                "7",
+             "Indentation",
+               "lambda",
+             "Pip Installs Packages"
+           
         ]
     },
     {
@@ -39,17 +71,34 @@ topics_data = [
         "questions": [
             "What is the only country that borders the United Kingdom?",
             "Ninety percent of the Earth's population lives in which hemisphere?",
-            # Add more questions as needed
+            "In which country would you find the city of Dresden?",
+               "In which country would you find the city of Dresden?",
+              "What would you call someone from Sudan?",
+            "What are the names of South Africa's three capital cities?",
+            "How many large islands make up Hawaii?",
+           "In which U.S. state can the world's tallest trees be found?",
+            "What is the name of the highest uninterrupted waterfall in the world?",
+           "Which river flows through the Grand Canyon?",
+           "What is the largest ocean in the world?"
+   	
         ],
         "answers": [
             "Ireland",
             "The Northern Hemisphere",
-            # Add corresponding answers
+             "The Northern Hemisphere",
+              "Germany",
+             "Sudanese",
+            "Cape Town, Pretoria, and Bloemfontein",
+           "Eight",
+            "California",
+           "Angel Falls",
+        "Colorado River",
+         "Pacific Ocean"
         ]
     },
 ]
 
-# Populate the database with topics, questions, and answers
+# Populating  the database with topics, questions, and answers
 for topic_data in topics_data:
     topic_obj, created = Topic.objects.get_or_create(name=topic_data["name"])
     if created:
